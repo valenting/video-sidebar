bd635f85faedad00bdf4ef99f2316013c3129121
uZZyBee@gmail.com <uZZyBee@gmail.com@bf5d6686-5b05-8f0a-0270-07812300dce9>
2010-07-08 12:11:04 +0000

    v0.1 cu playlist

---
+++ vlc01.valentin.gosu@gmail.com/chrome/content/vlc-controls.js
@@ -0,0 +1,100 @@
+       var vlc = document.getElementById("vlc");
+	   
+	   var items_list=new Array();
+	   var items_tags=new Array();
+	   var items_count=0;
+	   
+       function mute(){
+          vlc.audio.toggleMute();
+       }
+
+	   
+       function play(){
+          vlc.playlist.play();
+          intval = setInterval(uptime, 250);
+       }
+	   function fullscreen(){
+          vlc.video.toggleFullscreen();
+       }
+       function stop(){
+          clearInterval(intval);
+          vlc.playlist.stop();
+
+       }
+       function pause(){   
+          vlc.playlist.togglePause();
+       }
+       function c_time(){
+          return eval(vlc.input.time / 1000);
+       }
+       function seek(value){
+       var vlc = document.getElementById("vlc");
+          //alert(value);
+          vlc.input.time = eval(value*1000)
+          //return true;
+       }
+	   
+       function backof(value){
+       var vlc = document.getElementById("vlc");
+          vlc.input.time = vlc.input.time - eval(value*1000);
+          //alert(vlc.input.state);
+          //return true;
+       }
+	   
+	   function next()
+       {
+                vlc.playlist.next();
+       }
+	   
+	   function getplaylist()
+       {
+				var playlist=document.getElementById("playlist");
+				playlist.innerHTML = vlc.playlist.items.count+"<br/>";
+				var i=0;
+				for (i=0;i<items_count;i++) {
+					playlist.innerHTML+=items_list[i]+"<br/>";
+				}
+       }
+	   
+       /*function createplaylist()
+       {
+                var id1 = vlc.playlist.add("file:///home/stefan/Desktop/test/test.avi");
+                var id2 = vlc.playlist.add("file:///home/stefan/Desktop/test/test2.avi");
+                vlc.playlist.playItem(id1);
+       }*/
+	   
+	   function addToPlaylist(link) {
+			var id=vlc.playlist.add('file:///'+link);
+			//vlc.playlist.playItem(id);
+			items_list[items_count] = id;
+			items_tags[items_count] = link;
+			items_count++;
+			//playMov(link);
+	   }
+	   
+	   function playMov(item) {
+			var i;
+			//alert(items_count);
+			for (i=0;i<items_count;i++) {
+				//alert(items_tags[i]);
+				//alert(item);
+				if (items_tags[i]==item) {
+					vlc.playlist.playItem(items_list[i]);
+					break;
+				}
+			}
+	   }
+
+
+       function uptime(){
+          document.getElementById("nowt").innerHTML = eval(vlc.input.time/1000);
+       }
+       function else_r(){
+          oi = document.getElementById("txtout");
+          oi.value = '';
+          oi.value += vlc.versionInfo() +"\n";
+          oi.value += "VLC position " + vlc.input.position +"\n";
+          oi.value += "VLC length " + vlc.input.length +"\n";
+          oi.value += "VLC rate " + vlc.input.rate +"\n";
+          oi.value += "VLC is Playing " + vlc.playlist.isPlaying +"\n";
+       }
