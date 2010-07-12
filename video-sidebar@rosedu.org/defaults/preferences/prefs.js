2ec5e6d23e185d14098d07cd2d43b90d2ce5c7f4
andrei.maruseac <andrei.maruseac@bf5d6686-5b05-8f0a-0270-07812300dce9>
2010-07-12 10:34:19 +0000

    Test commit. Ignoraţi acest fişier

--- video-sidebar@rosedu.org/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.video-sidebar.boolpref", false);
-pref("extensions.video-sidebar.intpref", 0);
-pref("extensions.video-sidebar.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.video-sidebar@rosedu.org.description", "chrome://video-sidebar/locale/overlay.properties");
