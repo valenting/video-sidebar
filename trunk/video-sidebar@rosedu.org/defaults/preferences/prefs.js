1527c91b13a92f54b1fee5af0790ca422818b0c4
valentin.gosu <valentin.gosu@bf5d6686-5b05-8f0a-0270-07812300dce9>
2010-07-15 13:45:14 +0000

    new preferences/prefs.js

--- trunk/video-sidebar@rosedu.org/defaults/preferences/prefs.js
+++ trunk/video-sidebar@rosedu.org/defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 pref("extensions.video-sidebar.boolpref", false);
 pref("extensions.video-sidebar.intpref", 0);
-pref("extensions.video-sidebar.stringpref", "A string");
+pref("extensions.video-sidebar.stringpref", "q");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.video-sidebar@rosedu.org.description", "chrome://video-sidebar/locale/overlay.properties");
