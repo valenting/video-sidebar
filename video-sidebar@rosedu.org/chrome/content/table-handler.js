f6cc8909d7dbf745082efd9ad6f42e10572cf4e1
valentin.gosu <valentin.gosu@bf5d6686-5b05-8f0a-0270-07812300dce9>
2010-07-23 22:35:00 +0000

    Happy 100th commit Alpha go ahead "damn it, I feel like I'm talking to myself"

--- video-sidebar@rosedu.org/chrome/content/table-handler.js
+++ video-sidebar@rosedu.org/chrome/content/table-handler.js
@@ -17,10 +17,17 @@ function addRowToTable()
 	  el.appendChild(X);
 	  //el.setAttribute('onclick', );
 	  //el.size = 40;
+<<<<<<< .mine
+	  var filename = file.split('\\').pop().split('/').pop();
+	  if (filename.length>30)
+			filename = filename.substr(0,28)+'...';
+	  var textLink = document.createTextNode(filename);
+=======
 	  var filename = file.split('\\').pop().split('/').pop();
 	  if (filename.length>30)
 		filename = filename.substr(0,28)+'...';
 	  var textLink = document.createTextNode(filename);
+>>>>>>> .r99
 	  el.appendChild(textLink);
 	  
 	  //el.onkeypress = keyPressTest;
