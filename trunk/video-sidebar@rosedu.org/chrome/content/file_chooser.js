378d09be7c795d69f844ecd5f8a11bbc66d641c1
valentin.gosu <valentin.gosu@bf5d6686-5b05-8f0a-0270-07812300dce9>
2010-09-10 12:09:30 +0000

    -- code reorganize

--- trunk/video-sidebar@rosedu.org/chrome/content/file_chooser.js
+++ trunk/video-sidebar@rosedu.org/chrome/content/file_chooser.js
@@ -75,3 +75,36 @@ jQuery.fn.file = function() {
 		update();
 	});
 };
+
+
+function addRowToTable()
+{
+	var f=document.getElementById('add_file');
+	var file = f.value;
+	//file = file.toLowerCase();
+	if(file == "") {
+		alert("Enter a file name or click Open");
+	}
+	else {
+	  var tbl = document.getElementById('sortable');
+	  var el = document.createElement('li');
+	  el.id = 'txtRow' + file;
+	  el.setAttribute('class', 'ui-state-default');
+	  el.setAttribute('onDblclick', 'javascript:playMov("' + file.replace(/\\/g,"\\\\") + '")');
+	  var X = document.createElement('span');
+	  X.setAttribute('class', 'ui-icon ui-icon-close');
+	  el.appendChild(X);
+	  //el.setAttribute('onclick', );
+	  //el.size = 40;
+	  var filename = file.split('\\').pop().split('/').pop();
+	  if (filename.length>30)
+		filename = filename.substr(0,28)+'...';
+	  var textLink = document.createTextNode(filename);
+	  el.appendChild(textLink);
+	  
+	  //el.onkeypress = keyPressTest;
+	  tbl.appendChild(el);
+	  f.value='';
+	  addToPlaylist(file);
+	}
+}
